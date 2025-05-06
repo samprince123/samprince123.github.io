294b79d2f447f33cb10a8404a05242fd2c626cfc
samprince123 <sprince@csustan.edu>
2025-05-06 09:08:55 -0700

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -25,7 +25,7 @@ const CONFIG = {
       },
       manual: {
         // Properties for manually specifying projects
-        projects: ['microbiomeanalysis', 'genomeanalysis'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+        projects: ['samprince123/microbiomeanalysis', 'samprince123/genomeanalysis'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
       },
     },
     external: {
